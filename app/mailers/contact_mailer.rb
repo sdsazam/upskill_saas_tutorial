afaa80d91a87ff189bcb9627a733a712b6d56be1
Sienna Salo <siennasalo@gmail.com>
2017-05-26 15:58:14 +0000

    fixed contact_mailer.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,9 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'siennasalo@gmail.com'
-    
-    def contact_email(name, email, body)
-      @name = name
-      @email = email
-      @body = body
-      
-      mail(from: email, subject: 'Contact Form Message')
-    end
+  default to: 'siennasalo@gmail.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
